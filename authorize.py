106b2a4453cdd9e0a8c8dbb39af4965fed003b01
Andrew Wong <wong.andrewko@gmail.com>
2022-08-05 16:11:55 -0700

    Tidy comment formatting/ambiguity in `authorize.py`

--- authorize.py
+++ authorize.py
@@ -22,22 +22,25 @@ with open(CONFIG_FILE) as f:
 with open(config[CONFIG_SECRETS_FILE_KEY]) as f:
     secrets = toml.load(f)
 
-# Space separated scope
+# Scope is a space separated string
 scope = quote(config[CONFIG_SCOPE_KEY])
 # The app id and client secret registered with Fitbit developer portal
 client_id = secrets[SECRETS_CLIENT_ID_KEY]
 client_secret = secrets[SECRETS_CLIENT_SECRETS_KEY]
 
-# Auth flow step 1: make code verifier and challenge.
+# Five step authorization flow as per:
+# https://dev.fitbit.com/build/reference/web-api/developer-guide/authorization/
+# Auth Flow Step 1: make code verifier and challenge.
 code_verifier = make_code_verifier()
 code_challenge = make_code_challenge(code_verifier)
-# Auth flow step 2 request authorization.
+
+# Auth Flow Step 2 and 3: request and receive authorization code.
 # This will prompt for a login and authorization in webbrowser.
 # On completion, it will return the user to localhost,
 # with a query string parameter containing the code challenge.
 retrieve_auth_code(client_id, scope, code_challenge)
 
-# Prompt for proceed
+# Prompts the user before proceeding
 auth_code = input(" Paste code challenge from URL query string.\n"
                     " e.g. between 'code=' and '#_=_' in\n"
                     " \n       \"https://myapp.com/callback?code=d62d6f5bdc13df79d9a5f#_=_\"\n"
@@ -49,7 +52,7 @@ secrets[SECRETS_AUTH_CODE_KEY] = auth_code
 with open(config[CONFIG_SECRETS_FILE_KEY], 'w') as f:
     toml.dump(secrets, f)
 
-# Exchange for access and refresh token
+# Auth Flow Step 4 and 5: exchange auth code for access/refresh tokens.
 tokens_dict = exchange_code_for_tokens(
     client_id,
     client_secret,
